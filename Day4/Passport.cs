2ec22e34522cffcb85fe539b37c757cf7f36a238
Gustav Kron <gustav@vindsrummet.com>
2020-12-05 16:31:25 +0100

    Day4 done Day5 added

---
+++ Day4/Passport.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace Day4
+{
+    class Passport
+    {
+        public string Byr { get; set; }
+        public string Iyr { get; set; }
+        public string Eyr { get; set; }
+        public string Hgt { get; set; }
+        public string Hcl { get; set; }
+        public string Ecl { get; set; }
+        public string Pid { get; set; }
+        public string Cid { get; set; }
+
+        public bool ValidateAllFields(Passport passport)
+        {
+            if (ValidByr(Int32.Parse(passport.Byr)) != true)
+                return false;
+            else if (ValidIyr(Int32.Parse(passport.Iyr)) != true)
+                return false;
+            else if (ValidEyr(Int32.Parse(passport.Eyr)) != true)
+                return false;
+            else if (ValidHgt(passport.Hgt) != true)
+                return false;
+            else if (ValidateHcl(passport.Hcl) != true)
+                return false;
+            else if (ValidEcl(passport.Ecl) != true)
+                return false;
+            else if (ValidatePid(passport.Pid) != true)
+                return false;
+            return true;
+        }
+
+        private bool ValidByr(int byr)
+        {
+            bool returnValue = false;
+            if (byr >= 1920 && byr <= 2002)
+                returnValue = true;
+            return returnValue;
+        }
+
+        private bool ValidIyr(int iyr)
+        {
+            bool returnValue = false;
+            if (iyr >= 2010 && iyr <= 2020)
+                returnValue = true;
+            return returnValue;
+        }
+
+        private bool ValidEyr(int eyr)
+        {
+            bool returnValue = false;
+            if (eyr >= 2020 && eyr <= 2030)
+                returnValue = true;
+            return returnValue;
+        }
+
+        private bool ValidHgt(string hgt)
+        {
+            int hgtAsInt = ConvertHgtToInt(hgt);
+            if (hgtIsCm(hgt) && hgtAsInt >= 150 && hgtAsInt <= 193)
+                return true;
+            else if (hgtIsIn(hgt) && hgtAsInt >= 59 && hgtAsInt <= 76)
+                return true;
+            else
+                return false;
+
+
+        }
+        private bool ValidateHcl(string hcl)
+        {
+            bool returnValue = false;
+            Regex regex = new Regex("#{1}[a-f0-9]{6}");
+            if (regex.IsMatch(hcl))
+                returnValue = true;
+            return returnValue;
+        }
+
+        private bool ValidEcl(string ecl)
+        {
+            bool returnValue = false;
+            switch (ecl)
+            {
+                case "amb":
+                    returnValue = true;
+                    break;
+                case "blu":
+                    returnValue = true;
+                    break;
+                case "brn":
+                    returnValue = true;
+                    break;
+                case "gry":
+                    returnValue = true;
+                    break;
+                case "grn":
+                    returnValue = true;
+                    break;
+                case "hzl":
+                    returnValue = true;
+                    break;
+                case "oth":
+                    returnValue = true;
+                    break;
+            }
+            return returnValue;
+        }
+
+        private bool ValidatePid(string pid)
+        {
+            bool resturnValue = true;
+            if (WrongLengthOfPid(pid))
+                return false;
+            else
+            {
+                foreach (var myChar in pid)
+                {
+                    if (!Char.IsDigit(myChar))
+                        resturnValue = false;
+                }
+                return resturnValue;
+            }
+        }
+
+        private bool WrongLengthOfPid(string pid)
+        {
+            bool returnValue = false;
+            if (pid.Length != 9)
+                returnValue = true;
+            return returnValue;
+        }
+
+        private int ConvertHgtToInt(string hgt)
+        {
+            string height = "";
+            foreach (var myChar in hgt)
+            {
+                if (Char.IsDigit(myChar))
+                    height += myChar;
+            }
+            return Int32.Parse(height);
+        }
+
+        private bool hgtIsCm(string hgt)
+        {
+            bool returnValue = false;
+            Regex regex = new Regex("[c][m]$");
+            if (regex.IsMatch(hgt))
+                returnValue = true;
+            return returnValue;
+        }
+        private bool hgtIsIn(string hgt)
+        {
+            bool returnValue = false;
+            Regex regex = new Regex("[i][n]$");
+            if (regex.IsMatch(hgt))
+                returnValue = true;
+            return returnValue;
+        }
+    }
+
+
+
+
+}
--- Day4/Program.cs
+++ Day4/Program.cs
@@ -1,18 +1,128 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
 
 namespace Day4
-{
     class Program
     {
+        public static List<Passport> MyPassportList { get; set; } = new List<Passport>();
         static void Main(string[] args)
         {
             string puzzleSearchPath = (@"C:\Users\gusta\source\repos\AdventOfCode2020\Day4\puzzleInput.txt");
             string[] puzzleInput = System.IO.File.ReadAllLines(puzzleSearchPath);
+            string[] separatedPassportInfo = SeparatePassportInfo(CleanPassportInfo(puzzleInput));
+            MakePassportObjects(separatedPassportInfo);
+            Console.WriteLine(CountValidPassports());
         }
 
-        private static void MyMethod()
+        private static string CleanPassportInfo(string[] puzzleInput)
         {
+            string myNewString = "";
+            foreach (var line in puzzleInput)
+            {
+                if (line == "")
+                    myNewString += " *** ";
+                else
+                    myNewString += line + " ";
+            }
+            return myNewString;
+        }
 
+        private static string[] SeparatePassportInfo(string myString)
+        {
+            string[] myNewString = myString.Split(" *** ");
+            return myNewString;
+        }
+
+        private static int CountValidPassports()
+        {
+            int validPassports = 0;
+            foreach (var passport in MyPassportList)
+            {
+                if (IsItAValidPassport(passport))
+                    if(passport.ValidateAllFields(passport))
+                        validPassports++;
+            }
+            return validPassports;
+        }
+
+        private static bool IsItAValidPassport(Passport passport)
+        {
+            foreach (var propp in passport.GetType().GetProperties())
+            {
+                string value = (string)propp.GetValue(passport);
+                if (value == null && propp.Name != "Cid")
+                    return false;
+            }
+            return true;
+        }
+
+        private static void MakePassportObjects(string[] separatedPassportInfo)
+        {
+            foreach (var passport in separatedPassportInfo)
+            {
+                Passport myPassport = new Passport();
+                string[] myPassportInfo = passport.Split(new string[] { " ", "  "}, StringSplitOptions.RemoveEmptyEntries);
+                AddKeysAndValues(myPassportInfo);
+            }
+        }
+
+        private static void AddKeysAndValues(string[] myPassportInfo)
+        {
+            Passport myPassport = new Passport();
+            foreach (var info in myPassportInfo)
+            {
+                string key = GetKey(info);
+                string value = GetValue(info);
+                AddToMyPassport(key, value, myPassport);
+            }
+            MyPassportList.Add(myPassport);
+        }
+
+        private static string GetKey(string info)
+        {
+            return info.Substring(0, info.IndexOf(":"));
+        }
+
+        private static string GetValue(string info)
+        {
+            string sub = info.Substring((info.IndexOf(":")+1));
+            return sub;
+        }
+
+        private static void AddToMyPassport(string key, string value, Passport myPassport)
+        {
+            switch (key)
+            {
+                case "byr":
+                    myPassport.Byr = value;
+                    break;
+                case "iyr":
+                    myPassport.Iyr = value;
+                    break;
+                case "eyr":
+                    myPassport.Eyr = value;
+                    break;
+                case "hgt":
+                    myPassport.Hgt = value;
+                    break;
+                case "hcl":
+                    myPassport.Hcl = value;
+                    break;
+                case "ecl":
+                    myPassport.Ecl = value;
+                    break;
+                case "pid":
+                    myPassport.Pid = value;
+                    break;
+                case "cid":
+                    myPassport.Cid = value;
+                    break;
+                default:
+                    break;
+            }
         }
     }
 }
+
